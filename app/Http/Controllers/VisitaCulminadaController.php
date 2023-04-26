8c9c965dccc5b1e28fda2702c371e3c238cc2cf6
IvanCA <104946207+NaviCA21@users.noreply.github.com>
2023-04-26 15:27:43 -0500

    migracione sy controladores

---
+++ app/Http/Controllers/VisitaCulminadaController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\VisitaCulminada;
+use Illuminate\Http\Request;
+
+class VisitaCulminadaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(VisitaCulminada $visitaCulminada)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(VisitaCulminada $visitaCulminada)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, VisitaCulminada $visitaCulminada)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(VisitaCulminada $visitaCulminada)
+    {
+        //
+    }
+}
