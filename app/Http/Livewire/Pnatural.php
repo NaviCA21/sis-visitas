d7648667c21956dc944f5fe527a561515666ebba
felixrp74 <felixrp74@gmail.com>
2023-05-21 22:26:17 -0500

    horario natural y juridica completado

--- app/Http/Livewire/Pnatural.php
+++ app/Http/Livewire/Pnatural.php
@@ -14,8 +14,7 @@ class Pnatural extends Component
     public $search = '';
 
     public $fecha_live_wire = '2023-05-24';
-
-    // public $horarios = array('09:00:00', '10:00:00', '11:00:00');
+  
 
     public function render()
     {
@@ -24,35 +23,30 @@ class Pnatural extends Component
         $lista_horas_ocupadas = Periodo::where('fecha', '=', $this->fecha_live_wire)->get();
 
 
-        $horarios_totales = array('09:00:00', '10:00:00', '11:00:00');
+        $horarios_totales_mananitas = array('09:00:00', '10:00:00', '11:00:00');
+        $horarios_totales_tardecitas = array('14:00:00', '15:00:00');
+
+        $dayOfWeek = date("l", strtotime( $this->fecha_live_wire));
 
         $horarios_ocupados = array();
 
-        $i = 0;
 
-        // dd(count($lista_horas_ocupadas));
-
-        foreach ($lista_horas_ocupadas as $item) {
-            // $horarios_ocupados[$i++] = $item->horario_inicio;
-            // dd($item->horario_inicio);
+        foreach ($lista_horas_ocupadas as $item) { 
             array_push($horarios_ocupados, $item->hora_inicio);
-            // dd ("como las mairposas");
         }
+ 
+        if($dayOfWeek == 'Tuesday' ){
+            
+            $horarios_libres = array_diff($horarios_totales_mananitas, $horarios_ocupados);
+        }
+        else if($dayOfWeek == 'Thursday'){
 
-        // dd($horarios_ocupados);
-        // dd($horarios_totales);
-
-        $horarios_libres = array_diff($horarios_totales, $horarios_ocupados);
-
-        // dd($horarios_libres);
-
-        // echo 'horarios_libre '. $horarios_libres[0];
-        // echo 'horarios_libre '. $horarios_libres[1];
-        // echo 'horarios_libre '. $horarios_libres[2];
-
-
-
-
+            $horarios_libres = array_diff($horarios_totales_tardecitas, $horarios_ocupados);
+        }else {
+            $horarios_libres = array('00:00:00');
+        }
         return view('livewire.pnatural', compact('lista_horas_ocupadas', 'horarios_libres'));
+
+
     }
 }
